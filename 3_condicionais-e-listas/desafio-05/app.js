faf95ead25e2e68554ea82e2433cdc6018003a3e
Julio Saraiva <contato@juliosaraiva.com.br>
2019-07-23 20:19:24 -0300

    Renderizacao condicional

---
+++ 3_condicionais-e-listas/desafio-05/app.js
@@ -0,0 +1,18 @@
+const desafio = new Vue({
+	el: '#desafio',
+	data: {
+		seen: true,
+		array: ['Pedro', 'Bia', 'Ana', 'Rebeca'],
+		livro: {
+			titulo: 'O Senhor dos Anéis',
+			autor: 'J.R.R. Tolkiens',
+			volume: '3'
+		},
+		aluno: {
+			id: 10,
+			nome: 'Maria',
+			notas: [7.67, 8.33, 6.98, 9.21]
+		},
+		languages: ['Vue.JS', 'HTML', 'CSS', 'Node.JS'],
+	},
+});
