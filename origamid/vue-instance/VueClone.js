725260ec601de89234bcd271c5ad0ec8aeaa8640
Julio Saraiva <contato@juliosaraiva.com.br>
2019-08-10 05:25:38 -0300

    Documentado a Vue Instance

--- origamid/vue-instance/VueClone.js
+++ origamid/vue-instance/VueClone.js
@@ -1,12 +1,22 @@
-function VueClone(options) {
-  this.options = options;
-  this.el = document.querySelector(options.el);
-  this.data = options.data;
+class VueClone {
+  constructor(options) {
+    this.options = options;
+    this.el = document.querySelector(options.el);
+    this.data = options.data;
+  }
 }
 
-const vm = new VueClone({
+const vmClone = new VueClone({
   el: "#app",
   data: {
-    nome: "Martelo"
+    mensagem: "Martelo"
+  }
+});
+
+const vm = new Vue({
+  el: ".container",
+  data: {
+    instrumento: "Guitarra",
+    mensagem: "Hello World"
   }
 });
