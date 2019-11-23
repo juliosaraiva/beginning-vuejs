7e8e43ec178d161bb28419fdfac170bf299224d9
Julio Saraiva <contato@juliosaraiva.com.br>
2019-11-22 21:42:04 -0300

    transition

--- vue-origamid/origamid/animacoes/transition.vue
+++ vue-origamid/origamid/animacoes/transition.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <button @click="ativo = !ativo">Ativar</button>
-    <transition appear name="fade" enter-active-class="animation">
-      <h1>Serviços</h1>
+    <button @click="ativo = !ativo">Ativar Transiction</button>
+    <transition appear name="fade">
+      <h1>Transições no Vue</h1>
     </transition>
   </div>
 </template>
@@ -12,19 +12,20 @@ export default {
   data() {
     return {
       ativo: false
-    };
+    }
   }
-};
+
+}
 </script>
 
-
-
 <style>
-.fade-enter {
+/* .fade-enter-active, .fade-leave-active {
+  transition: 1.5s;
+} */
+.fade-enter, .fade-leave-to {
   opacity: 0;
 }
-
-.animation {
-  transition: opacity 2s;
+.fade-enter-active, .fade-leave-active {
+  transition: 5s;
 }
 </style>
